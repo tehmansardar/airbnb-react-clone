0ea9bc8bc313c891b9e2b044ef928be85a876a9d
Tehman Sardar <tehmansardar@gmail.com>
2020-12-17 02:35:49 +0500

    Project Initalized

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import {Avatar, Button} from '@material-ui/core';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Hell world</h1>
+    </div>
+  );
+}
+
+export default App;
